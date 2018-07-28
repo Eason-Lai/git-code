6dc481fee82f389966a6cec2fda08ebed57aaef4
黎奕汛 <liyixun@liyixundeMacBook-Pro.local>
2018-07-28 13:27:04 +0800

    learn git

---
+++ vscode/c++/test.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n, m;
+    cout << "input" << endl;
+    cin >> n;
+    cout << "n=" << n * 8 << endl;
+    cout << 1 << endl;
+    cin >> n;
+    cout << "n=" << n * 8 << endl;
+    return 0;
+}
