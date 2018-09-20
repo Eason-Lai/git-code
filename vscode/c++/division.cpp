e8f5125416e5294333f3e2668594c757bee8e68d
黎奕汛 <liyixun@liyixundeMacBook-Pro.local>
2018-09-20 10:00:38 +0800

    2

---
+++ vscode/c++/division.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+const int slength = 15;
+vector <char[slength]> v;
+void scan(char *s, int &index)
+{
+    char temp[slength];
+    //去空格，直到指针指向非空格字符串
+    
+}
+int main()
+{
+    return 0;
+}
--- vscode/c++/scan.cpp
+++ vscode/c++/scan.cpp
@@ -2,47 +2,98 @@
 #include <cstdio>
 #include <string>
 using namespace std;
-//判断是否为空格，是的话index前移再判断下一位
-bool IsSpace(char *s, int &index) 
-{
-    int i=index;
-    while ( s[index] == ' ' ) {
-        index++;
-    }  
-    if ( i == index ) return true;
-    else return false;
-}
-//判断是否为特殊符号
-char* IsSymbol(char *s, int &index)
-{
-    char* ns=new char[5];
 
-}
-//判断是否为数字（常数）
-char* IsNumber(char *s, int &index)
+//判断单个字符
+bool IsNum(char c) //数字
 {
-    char* ns=new char[10];
-    int k=0;
-    while ( s[index] >= 48 && s[index] <= 57 ) {  //开头为0-9或者后面含有0-9
-        ns[k]=s[index];
+    return (c >= 48 && c <= 57);
+}
+bool IsLowercase(char c) //小写字母
+{
+    return (c >= 97 && c <= 122);
+}
+bool IsCapital(char c) //大写字母
+{
+    return (c >= 65 && c <= 90);
+}
+bool Isalphabet(char c) //字母
+{
+    return (IsLowercase(c) || IsCapital(c));
+}
+
+//判断是否为空格，是的话index前移再判断下一位
+bool IsSpace(char *s, int &index)
+{
+    int i = index;
+    while (s[index] == ' ')
+    {
+        index++;
+    }
+    if (i == index)
+        return true;
+    else
+        return false;
+}
+
+//判断是否为特殊符号
+char *IsSymbol(char *s, int &index)
+{
+    char *ns = new char[5];
+}
+
+//判断是否为数字（常数）
+char *IsNumber(char *s, int &index)
+{
+    char *ns = new char[10];
+    int k = 0;
+    while (IsNum(s[index])) //开头为0-9或者后面含有0-9
+    { 
+        ns[k] = s[index];
         k++;
         index++;
     }
-    if ( k != 0 ) cout<<ns<<"\t\t\t"<<"数"<<endl;
+    if (k != 0)
+        cout << ns << "\t\t\t"
+             << "数" << endl;
     return ns;
 }
+
+//判断是否为单词
+char *IsWord(char *s, int &index)
+{
+    char *ns = new char[15];
+    int k = 0;
+    if (IsLowercase(s[index]) || IsCapital(s[index])) //第一个字母
+    {
+        ns[k] = s[index];
+        k++;
+        index++;
+        while (IsNum(s[index]) || Isalphabet(s[index])) //开头为0-9或者后面含有0-9
+        { 
+            ns[k] = s[index];
+            k++;
+            index++;
+        }
+    }
+}
+
+//判断是否为串
+
 int main()
 {
     char str[200];
     int i;
     int length;
-    while ( cin.getline(str,200) ) {
-        if ( str[0] == '\0' ) break; //判断是否未最后一行
-        i=0;
-        length=strlen(str);
-        while ( i != length ) {
-            IsSpace(str,i);
-            IsNumber(str,i);
+    while (cin.getline(str, 200))
+    {
+        if (str[0] == '\0')
+            break; //判断是否未最后一行
+        i = 0;
+        length = strlen(str);
+        while (i != length)
+        {
+            IsSpace(str, i);
+            IsNumber(str, i);
         }
     }
     return 0;
